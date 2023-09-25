51ff4b777fcc662f0af8f8f29f58ad483a7d5ade
LAPTOP-DPS9GSGG\admin <khiendz@gmail.com>
2023-09-25 23:10:40 +0700

    DATN-2023-v2: feat: update lib

--- FE/client-app/src/app/layout.tsx
+++ FE/client-app/src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import LayoutDefault from "@/components/layouts/LayoutDefault";
+import type { PropsWithChildren } from 'react'
+import { RootStyleRegistry } from '../modules/shared/components/Root-style-registry';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <html lang="vn">
+    <head />
+    <body>
+      <RootStyleRegistry>{children}</RootStyleRegistry>
+    </body>
+  </html>
   );
 }
